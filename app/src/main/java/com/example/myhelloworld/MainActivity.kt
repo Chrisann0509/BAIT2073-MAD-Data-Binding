58abe6bf4cbd37ff7ef9d408796a91815c87f7ae
Chrisann0509 <chrisannlee97@gmail.com>
2022-07-12 18:02:54 +0800

    w2 version2

--- app/src/main/java/com/example/myhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/myhelloworld/MainActivity.kt
@@ -8,13 +8,20 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImg:ImageView
+   //var diceImg:ImageView=null  may cause exception eg. null exception
+    //var :value changeable
+    //val: value unchangeable
+    lateinit var numbText:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//     diceImg=findViewById(R.id.diceImg)  - cannot put above setContentView cuz haven't prepare view
         setContentView(R.layout.activity_main)
-
+        diceImg=findViewById(R.id.diceImg)
+        numbText=findViewById(R.id.numberTxt)
         val rollButton:Button=findViewById(R.id.rollBtn)
         //val rollButton=findViewById<Button>(R.id.rollBtn)
-        rollButton.setOnClickListener{rollDice()}
+        rollButton.setOnClickListener{rollDice()}  //prepare layout
     }
 
     private fun rollDice(){
@@ -22,9 +29,9 @@ class MainActivity : AppCompatActivity() {
       //  Toast.LENGTH_SHORT).show()
 
         val randomNum=(1..6).random()
-        val numberText: TextView =findViewById(R.id.numberTxt)
-        numberText.text=randomNum.toString()
-        val diceImg: ImageView =findViewById(R.id.diceImg)
+  //      val numberText: TextView =findViewById(R.id.numberTxt)
+          numbText.text=randomNum.toString()
+//        val diceImg: ImageView =findViewById(R.id.diceImg)
         val imgSrc=when(randomNum){
             1->R.drawable.dice_1
             2->R.drawable.dice_2
